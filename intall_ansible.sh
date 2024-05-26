dbeeac918d1a4ee9de01ecf269dd018e3e8cebac
Digo <digomattar@gmail.com>
2024-05-26 13:14:12 -0300

    feat: added python integration and second ansible playbook

--- intall_ansible.sh
+++ intall_ansible.sh
@@ -1,4 +1,31 @@
 #install via dnf in redhat
+sudo dnf install git -y
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+sudo pip3 install ansible -y
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube /usr/local/bin && sudo chmod +x /usr/local/bin/minikube
+sudo yum install libvirt libvirt-daemon-kvm qemu-kvm
+sudo systemctl enable --now libvirtd
+sudo usermod -a -G libvirt $(whoami)
+newgrp libvirt
+#minikube start --driver=kvm2
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+cd ansible_k8_api_db
 sudo pip install kubernetes
 ansible-galaxy collection install community.general kubernetes.core
+pip install -r requirements.txt
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+sudo subscription-manager repos --enable=rhel-9-for-x86_64-baseos-rpms
+sudo subscription-manager repos --enable=rhel-9-for-x86_64-appstream-rpms
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo yum install conntrack-tools -y
+minikube start --force
 
---
+++ main.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+def run_playbook(playbook_path):
+    try:
+        # Start the Ansible playbook process
+        with subprocess.Popen(['ansible-playbook', playbook_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as proc:
+            # Read output line by line as it becomes available
+            for line in proc.stdout:
+                print(line, end='')  # Print stdout in real-time
+
+            # Wait for the process to complete
+            proc.wait()
+
+            # Check if the process exited with an error
+            if proc.returncode != 0:
+                # Read and print stderr if there was an error
+                print("ERROR:", proc.stderr.read())
+
+    except Exception as e:
+        print(f"An unexpected error occurred while running the playbook {playbook_path}: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    playbook1 = "ansible_collect.yaml"
+    playbook2 = "ansible_minikube_deploy.yaml"
+    
+    run_playbook(playbook1)
+    run_playbook(playbook2)
