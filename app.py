061da36f627c44f734bedeacd0ffc8b96095878b
Digo <digomattar@gmail.com>
2024-06-05 18:27:44 -0300

    feat: rewmove pre seed for test

--- app.py
+++ app.py
@@ -17,24 +17,24 @@ class Movie(db.Model):
     director = db.Column(db.String(100), nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-    if Movie.query.count() == 0:
-        movies = [
-            Movie(title="Inception", director="Christopher Nolan", year=2010),
-            Movie(title="The Matrix", director="Lana Wachowski, Lilly Wachowski", year=1999),
-            Movie(title="Interstellar", director="Christopher Nolan", year=2014),
-            Movie(title="Parasite", director="Bong Joon Ho", year=2019),
-            Movie(title="The Shawshank Redemption", director="Frank Darabont", year=1994),
-            Movie(title="Pulp Fiction", director="Quentin Tarantino", year=1994),
-            Movie(title="The Godfather", director="Francis Ford Coppola", year=1972),
-            Movie(title="The Dark Knight", director="Christopher Nolan", year=2008),
-            Movie(title="Schindler's List", director="Steven Spielberg", year=1993),
-            Movie(title="Fight Club", director="David Fincher", year=1999)
-        ]
-        db.session.bulk_save_objects(movies)
-        db.session.commit()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
+#     if Movie.query.count() == 0:
+#         movies = [
+#             Movie(title="Inception", director="Christopher Nolan", year=2010),
+#             Movie(title="The Matrix", director="Lana Wachowski, Lilly Wachowski", year=1999),
+#             Movie(title="Interstellar", director="Christopher Nolan", year=2014),
+#             Movie(title="Parasite", director="Bong Joon Ho", year=2019),
+#             Movie(title="The Shawshank Redemption", director="Frank Darabont", year=1994),
+#             Movie(title="Pulp Fiction", director="Quentin Tarantino", year=1994),
+#             Movie(title="The Godfather", director="Francis Ford Coppola", year=1972),
+#             Movie(title="The Dark Knight", director="Christopher Nolan", year=2008),
+#             Movie(title="Schindler's List", director="Steven Spielberg", year=1993),
+#             Movie(title="Fight Club", director="David Fincher", year=1999)
+#         ]
+#         db.session.bulk_save_objects(movies)
+#         db.session.commit()
 
         
 @app.errorhandler(Exception)
